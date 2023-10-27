0b9fe2a2f312985c8f7c632ce0d7ea9bf35acd24
Streann Media <148217867+streannsdk@users.noreply.github.com>
2023-10-27 19:04:56 +0200

    Add files via upload

--- Sources/Files/Model/InsideAdCallbackTypeModel.swift
+++ Sources/Files/Model/InsideAdCallbackTypeModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum InsideAdCallbackType {
+public enum InsideAdCallbackType: Equatable, CaseIterable {
     case AD_BREAK_READY
     case AD_BREAK_FETCH_ERROR
     case AD_BREAK_ENDED
@@ -34,4 +34,37 @@ public enum InsideAdCallbackType {
     case THIRD_QUARTILE
     case UNKNOWN
     case IMAAdError(String)
+    
+    public static var allCases: [InsideAdCallbackType] {
+        return [
+            .AD_BREAK_ENDED,
+            .AD_BREAK_FETCH_ERROR,
+            .AD_BREAK_ENDED,
+            .AD_BREAK_STARTED,
+            .AD_PERIOD_ENDED,
+            .AD_PERIOD_STARTED,
+            .ALL_ADS_COMPLETED,
+            .CLICKED,
+            .COMPLETE,
+            .CUEPOINTS_CHANGED,
+            .ICON_FALLBACK_IMAGE_CLOSED,
+            .ICON_TAPPED,
+            .FIRST_QUARTILE,
+            .LOADED,
+            .LOG,
+            .MIDPOINT,
+            .PAUSE,
+            .RESUME,
+            .SKIPPED,
+            .STARTED,
+            .STREAM_LOADED,
+            .STREAM_STARTED,
+            .TAPPED,
+            .THIRD_QUARTILE,
+            .UNKNOWN,
+            .IMAAdError("")
+        ]
+    }
 }
+
+
