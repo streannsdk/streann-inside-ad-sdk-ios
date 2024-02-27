c01a5b52dae0a24752838aabc5533b54820f5e46
Streann Media <148217867+streannsdk@users.noreply.github.com>
2024-02-27 14:15:07 +0100

    Add files via upload

--- Sources/Files/SDK/View/LocalImageView.swift
+++ Sources/Files/SDK/View/LocalImageView.swift
@@ -8,33 +8,38 @@
 import SwiftUI
 
 struct LocalImageView: View {
+    @StateObject var imageLoader: ImageLoaderService
+    @State var image: UIImage = UIImage()
     var insideAd: InsideAd
     @Binding var insideAdCallback: InsideAdCallbackType
     
     public init(insideAd: InsideAd, insideAdCallback: Binding<InsideAdCallbackType>) {
         self._insideAdCallback = insideAdCallback
         self.insideAd = insideAd
+        self._imageLoader = StateObject(wrappedValue: ImageLoaderService(insideAdCallback: insideAdCallback))
     }
     
     var body: some View {
         ZStack {
-            AsyncImage(url: URL(string: insideAd.url ?? "")) { phase in
-                if let image = phase.image {
-                    image
-                        .frame(maxWidth: UIScreen.main.bounds.width)
-                        .scaledToFit()
-                        .onAppear(perform: {
-                            insideAdCallback = .LOADED
-                        })
+            Image(uiImage: image)
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: UIScreen.main.bounds.width)
+                .onReceive(imageLoader.$image) { image in
+                    self.image = image
+                    insideAdCallback = .LOADED
                 }
-            }
-            
+                .onAppear {
+                    if let url = insideAd.url {
+                        imageLoader.loadImage(for: url)
+                    }
+                }
             VStack{
                 topView
                 Spacer()
             }
             .padding(2)
         }
+        .hide(if: insideAdCallback == .ALL_ADS_COMPLETED)
     }
 }
 
@@ -67,5 +72,30 @@ extension LocalImageView {
                     .foregroundStyle(.white)
             })
         }
+    }
+}
+
+
+class ImageLoaderService: ObservableObject {
+    @Published var image: UIImage = UIImage()
+    @Binding var insideAdCallback: InsideAdCallbackType
+    
+    init(insideAdCallback: Binding<InsideAdCallbackType>) {
+        self._insideAdCallback = insideAdCallback
+    }
+    
+    func loadImage(for urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                self.insideAdCallback = .IMAAdError(error?.localizedDescription ?? "")
+                return
+            }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: data) ?? UIImage()
+            }
+        }
+        task.resume()
     }
 }
