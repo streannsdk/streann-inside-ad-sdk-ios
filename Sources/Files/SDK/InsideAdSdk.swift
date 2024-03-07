721806519094d0f727977104260ac1726a74aec9
Streann Media <148217867+streannsdk@users.noreply.github.com>
2024-03-07 13:08:09 +0100

    Add files via upload

--- Sources/Files/SDK/InsideAdSdk.swift
+++ Sources/Files/SDK/InsideAdSdk.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 public class InsideAdSdk {
+    public static let shared = InsideAdSdk()
+    
     public init(baseUrl: String,
                 apiKey: String,
                 apiToken: String,
@@ -16,7 +18,7 @@ public class InsideAdSdk {
                 storeUrl: String? = nil,
                 descriptionUrl: String? = nil,
                 userBirthYear: Int64? = nil,
-                userGender: UserGender? = nil) {
+userGender: UserGender? = nil) {
         Constants.ResellerInfo.baseUrl = baseUrl
         Constants.ResellerInfo.apiKey = apiKey
         Constants.ResellerInfo.apiToken = apiToken
@@ -26,7 +28,10 @@ public class InsideAdSdk {
         Constants.ResellerInfo.descriptionUrl = descriptionUrl ?? ""
         Constants.UserInfo.userBirthYear = userBirthYear
         Constants.UserInfo.userGender = userGender ?? .unknown
+        _ = CampaignManager.shared
     }
+    
+    public init() { }
     
     @ViewBuilder
     public func insideAdView(screen: String, playerState: Binding<InsideAdCallbackType>, isAdMuted: Bool = false) -> some View {
@@ -49,36 +54,38 @@ public class InsideAdSdk {
         }
         
         var body: some View {
-            InsideAdView(screen: screen, insideAdCallback: playerState)
-                .id(adViewId)
-                .frame(maxWidth: adViewWidth, maxHeight: adViewHeight)
-                .onReceive(NotificationCenter.default.publisher(for: .AdsContentView_setFullSize), perform: { _ in
-                    adViewHeight = UIScreen.main.bounds.width / 16 * 9
-                    adViewWidth = .infinity
-                })
-                .onReceive(NotificationCenter.default.publisher(for: .AdsContentView_setZeroSize), perform: { _ in
-                    adViewHeight = 0
-                    adViewWidth = 0
-                })
-                .onReceive(NotificationCenter.default.publisher(for: .AdsContentView_startTimer), perform: { _ in
-                    var intervalInMinutes = Constants.ResellerInfo.intervalInMinutes
-                    if let intervalInMinutesCamp = Constants.CampaignInfo.intervalInMinutes {
-                        intervalInMinutes = intervalInMinutesCamp
-                    }
-                    
-                    if let intervalInMinutes, intervalInMinutes > 0 {
-                        print(Logger.log("Timer started for next ad - intervalInMinutes \(intervalInMinutes)"))
-                        timerNextAd = Timer.scheduledTimer(withTimeInterval: TimeInterval(intervalInMinutes.convertMinutesToSeconds()), repeats: false){ _ in
-                            adViewId = UUID()
+            VStack {
+                InsideAdView(screen: screen, insideAdCallback: playerState)
+                    .id(adViewId)
+                    .frame(maxWidth: adViewWidth, maxHeight: adViewHeight)
+                    .onReceive(NotificationCenter.default.publisher(for: .AdsContentView_setFullSize), perform: { _ in
+                        adViewHeight = UIScreen.main.bounds.width / 16 * 9
+                        adViewWidth = .infinity
+                    })
+                    .onReceive(NotificationCenter.default.publisher(for: .AdsContentView_setZeroSize), perform: { _ in
+                        adViewHeight = 0
+                        adViewWidth = 0
+                    })
+                    .onReceive(NotificationCenter.default.publisher(for: .AdsContentView_startTimer), perform: { _ in
+                        var intervalInMinutes = Constants.ResellerInfo.intervalInMinutes
+                        if let intervalInMinutesCamp = CampaignManager.shared.allCampaigns?.getActiveCampaign()?.properties?.intervalInMinutes {
+                            intervalInMinutes = intervalInMinutesCamp
                         }
-                    }else{
-                        print(Logger.log("Timer not started - intervalInMinutes \(intervalInMinutes ?? 0)"))
-                    }
-                })
-                .onReceive(NotificationCenter.default.publisher(for: .AdsContentView_stopTimer), perform: { _ in
-                    timerNextAd?.invalidate()
-                    timerNextAd = nil
-                })
+                        
+                        if let intervalInMinutes, intervalInMinutes > 0 {
+                            print(Logger.log("Timer started for next ad - intervalInMinutes \(intervalInMinutes)"))
+                            timerNextAd = Timer.scheduledTimer(withTimeInterval: TimeInterval(intervalInMinutes.convertMinutesToSeconds()), repeats: false){ _ in
+                                adViewId = UUID()
+                            }
+                        }else{
+                            print(Logger.log("Timer not started - intervalInMinutes \(intervalInMinutes ?? 0)"))
+                        }
+                    })
+                    .onReceive(NotificationCenter.default.publisher(for: .AdsContentView_stopTimer), perform: { _ in
+                        timerNextAd?.invalidate()
+                        timerNextAd = nil
+                    })
+            }
         }
     }
 }
