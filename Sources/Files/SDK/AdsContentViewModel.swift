5a6ad6a73f31016858f8f441220ffa9efb9baa25
Streann Media <148217867+streannsdk@users.noreply.github.com>
2024-04-18 10:44:34 +0200

    Add files via upload

---
+++ Sources/Files/SDK/AdsContentViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  AdsContentViewModel.swift
+//  TestTheLibrary
+//
+//  Created by Igor Parnadziev on 18.4.24.
+//
+
+import Foundation
+
+class AdsContentViewModel: ObservableObject{
+    
+    init(){
+        print("AdsContentViewModel init")
+    }
+}
